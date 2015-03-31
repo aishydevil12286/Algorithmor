f4ad3ceebe348688c5ee897bb3321fce34d02127
aishydevil12286 <aishydevil12286@gmail.com>
2015-03-31 19:59:20 +0530

    Some problems

---
+++ Algorithmor/src/patterns/GenericFactory.java
@@ -0,0 +1,15 @@
+package patterns;
+
+public class GenericFactory<E> {
+
+    Class theClass = null;
+
+    public GenericFactory(Class theClass) {
+        this.theClass = theClass;
+    }
+
+    public E createInstance()
+    throws IllegalAccessException, InstantiationException {
+        return (E) this.theClass.newInstance();
+    }
+}
