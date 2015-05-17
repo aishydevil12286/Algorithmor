d2115f2656753bc9d21ae0aa1831afdeae2c8afa
aishydevil12286 <aishydevil12286@gmail.com>
2015-05-17 13:59:52 +0530

    Misc

--- Algorithmor/src/ds/trees/bst/BinaryTreeTraversal.java
+++ Algorithmor/src/ds/trees/bst/BinaryTreeTraversal.java
@@ -3,7 +3,7 @@ package ds.trees.bst;
 public class BinaryTreeTraversal {
 	public Node root;
 	
-	public void inOrder(Node root){ // Left -> Root -> Right
+	public static void inOrder(Node root){ // Left -> Root -> Right
 		if(root == null){
 			return;
 		}
@@ -12,7 +12,7 @@ public class BinaryTreeTraversal {
 		inOrder(root.right);
 	}
 	
-	public void preOrder(Node root){ // Root -> Left -> Right
+	public static void preOrder(Node root){ // Root -> Left -> Right
 		if(root == null){
 			return;
 		}
@@ -21,7 +21,7 @@ public class BinaryTreeTraversal {
 		preOrder(root.right);
 	}
 	
-	public void postOrder(Node root){ // Left -> Right -> Root
+	public static void postOrder(Node root){ // Left -> Right -> Root
 		if(root == null){
 			return;
 		}
@@ -31,17 +31,15 @@ public class BinaryTreeTraversal {
 	}
 
 	public static void main(String[] args) {
-		BinaryTreeTraversal traverse = new BinaryTreeTraversal();
-		traverse.root = new Node('A');
-		traverse.root.left = new Node(new Node('D'),new Node('E'),'B');
-		traverse.root.right = new Node(new Node('F'),new Node('G'),'C');
+		Node root = new Node('A');
+		root.left = new Node(new Node('D'),new Node('E'),'B');
+		root.right = new Node(new Node('F'),new Node('G'),'C');
 		System.out.println("In Order Traversal");
-		traverse.inOrder(traverse.root);
+		inOrder(root);
 		System.out.println("\nPre Order Traversal");
-		traverse.preOrder(traverse.root);
+		preOrder(root);
 		System.out.println("\nPost Order Traversal");
-		traverse.postOrder(traverse.root);
-
+		postOrder(root);
 	}
 
 }
