1c8cd832f08d68d1ac5b264175932a3a0174fd01
aishydevil12286 <aishydevil12286@gmail.com>
2015-06-19 22:59:27 +0530

    Quick select and a O1 time LRU cache

---
+++ Algorithmor/src/algo/selection/QuickSelect.java
@@ -0,0 +1,15 @@
+package algo.selection;
+
+public class QuickSelect {
+	
+	public int quickSelect(int[] a,int order){
+		
+		return 0;
+	}
+
+	public static void main(String[] args) {
+	
+
+	}
+
+}
