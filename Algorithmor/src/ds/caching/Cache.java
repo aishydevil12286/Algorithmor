84246b8b2da86af50719dd2e366a870da171f49b
ash <aishydevil12286@gmail.com>
2017-05-23 20:00:47 +0530

    Exploring caching implementations

---
+++ Algorithmor/src/ds/caching/Cache.java
@@ -0,0 +1,34 @@
+package ds.caching;
+
+/*
+ * A cache interface can generically store values indexed by hashcode of keys. For object types that have 
+ * efficient hashCode methods already existing or 
+ */
+public interface Cache<K,V> {
+
+	/*
+	 * Adds the Key value pair in the cache
+	 */
+	public void add(K key,V data);
+	
+	/*
+	 * Returns the value associated with the key
+	 */
+	public V get(K key);
+	
+	/*
+	 * Boolean flag after checking whether cache is full
+	 */
+	//public boolean isFull();
+	
+	/*
+	 * Boolean flag after checking whether cache is empty
+	 */
+	//public boolean isEmpty();
+	
+	/*
+	 * clears the cache
+	 */
+	//public void clear();
+
+}
---
+++ Algorithmor/src/ds/caching/LRUCache.java
@@ -0,0 +1,5 @@
+package ds.caching;
+
+public class LRUCache {
+
+}
