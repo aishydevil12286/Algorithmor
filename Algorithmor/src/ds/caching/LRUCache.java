5d35ea6a8be183fcadccd6c09adcd28bf061eab3
ash <aishydevil12286@gmail.com>
2017-05-23 20:01:13 +0530

    Caching files

--- Algorithmor/src/ds/caching/LRUCache.java
+++ Algorithmor/src/ds/caching/LRUCache.java
@@ -1,5 +1,24 @@
 package ds.caching;
 
-public class LRUCache {
+import java.util.LinkedHashMap;
+
+public class LRUCache implements Cache<K,V>{
+	private Map<K,V> cache;
+	
+	public LRUCache(){
+		this.cache = new ConcurrentLinkedHashMap<>();
+	}
+
+	@Override
+	public void add(K key, V data) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public V get(K key) {
+		// TODO Auto-generated method stub
+		return null;
+	}
 
 }
